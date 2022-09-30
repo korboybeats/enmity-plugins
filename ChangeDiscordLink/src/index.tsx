3a419c460fbb135fc6e980458e2c7c33c22783bf
Rosie <jamsowo@outlook.com>
2022-09-30 17:25:18 +0100

    t

--- ChangeDiscordLink/src/index.tsx
+++ ChangeDiscordLink/src/index.tsx
@@ -12,12 +12,28 @@ const ChangeDiscordLink: Plugin = {
 
    onStart() {      
       Patcher.before(Messages, "sendMessage", (self, args, orig) => {
-      let customText = get('ChangeDiscordLink', "urlPrefix", "stable")
-        const content = args[1]["content"];
-      
-        if(!content.includes("discord.com/channels")) return;
-      
-        args[1]["content"] = content.replace("discord.com/channels", customText + ".discord.com/channels");
+         let customText = get('ChangeDiscordLink', "urlPrefix", "stable")
+         const formatString = (text: string) => {
+            let format1 = text.split(" ").map(e => e.toLowerCase()).join('-')
+            let format2: string[] = [];
+            let alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890-_'
+            format1.includes(alphabet) 
+                ? []
+                : format1.split('').map((char: string)=> {
+                    if (!alphabet.includes(char)) return format2.push('-')
+                    return format2.push(char)
+        
+                }).join('')
+        
+            return format2.join('')
+            
+        }
+        
+        console.log(formatString("Hello World ?a?")) // hello-world--a-
+
+         const content = args[1]["content"];
+         if(!content.includes("discord.com/channels")) return;
+         args[1]["content"] = content.replace("discord.com/channels", customText + ".discord.com/channels");
       });
    },
    onStop() {

fb2b6d60a5054128c4b2e44ab3358d524e0c2154
Rosie <jamsowo@outlook.com>
2022-09-30 18:56:42 +0100

    remove pointless shit

--- ChangeDiscordLink/src/index.tsx
+++ ChangeDiscordLink/src/index.tsx
@@ -14,17 +14,15 @@ const ChangeDiscordLink: Plugin = {
       Patcher.before(Messages, "sendMessage", (self, args, orig) => {
          let customText = get('ChangeDiscordLink', "urlPrefix", "stable")
          const formatString = (text: string) => {
-            let format1 = text.split(" ").map(e => e.toLowerCase()).join('-')
-            let format2: string[] = [];
             let alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890-_'
+            let format1 = text.split("").map(e => e.toLowerCase().replace(" ", "_")).join('')
+            let format2: string[] = [];
             format1.split('').map((char: string)=> {
                 if (!alphabet.includes(char)) return format2.push('-')
                 return format2.push(char)
             })
             return format2.join('')
         }
-        console.log(formatString("Hello World ?a?"))
-
          const content = args[1]["content"];
          if(!content.includes("discord.com/channels")) return;
          args[1]["content"] = content.replace("discord.com/channels", customText + ".discord.com/channels");
